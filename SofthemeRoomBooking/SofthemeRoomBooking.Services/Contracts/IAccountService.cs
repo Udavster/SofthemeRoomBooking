7ac86032643d6de4c6db5b0e77639fd8f6f89b04
Danil Shypik <udavster@gmail.com>
2016-11-24 17:59:41 +0200

    Add: controllers and services

---
+++ SofthemeRoomBooking/SofthemeRoomBooking.Services/Contracts/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SofthemeRoomBooking.Services.Contracts
+{
+    public interface IAccountService
+    {
+    }
+}
---
+++ SofthemeRoomBooking/SofthemeRoomBooking/Controllers/HRDepartmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SofthemeRoomBooking.Controllers
+{
+    public class HrDepartmentController : Controller
+    {
+        // GET: HRDepartment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
