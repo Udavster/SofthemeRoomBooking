7dc1e885db9d66889d90485ab99b4e37ac12f24c
Danil Shy <udavster@gmail.com>
2016-12-03 21:11:24 +0200

    Fix: db. Changed names to plural where possible.

--- SofthemeRoomBooking/Database/EquipmentRooms.sql
+++ SofthemeRoomBooking/Database/EquipmentRooms.sql
@@ -1,7 +1,10 @@
-﻿CREATE TABLE [dbo].[EquipmentRooms]
-(
+﻿CREATE TABLE [dbo].[EquipmentRooms](
 	id INT IDENTITY PRIMARY KEY,
-	id_room INT,
-	id_equipment INT,
-	quantity INT
+	id_room INT NOT NULL,
+	id_equipment INT NOT NULL,
+	quantity INT NOT NULL
+	CONSTRAINT fk_ERtoRoom FOREIGN KEY (id_room)
+		REFERENCES Rooms(id),
+	CONSTRAINT fk_ERtoEquipment FOREIGN KEY (id_equipment)
+		REFERENCES Equipment(id)
 )
