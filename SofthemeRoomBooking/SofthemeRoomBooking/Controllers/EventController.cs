6ffd135ddaf20405c6d7de9f471de05959fad026
multidila <nagayko.dima@mail.ru>
2016-12-15 15:56:55 +0200

    Create EventView (Demo)

--- SofthemeRoomBooking/SofthemeRoomBooking/Controllers/EventController.cs
+++ SofthemeRoomBooking/SofthemeRoomBooking/Controllers/EventController.cs
@@ -14,21 +14,22 @@ namespace SofthemeRoomBooking.Controllers
     {
         private readonly IEventService _eventService;
         private readonly IRoomService _roomService;
-        private readonly ApplicationUserManager _userManager;
         private readonly IProfileService _profileService;
 
         public EventController(IEventService eventService,IRoomService roomService, ApplicationUserManager userManager, IProfileService profileService)
         {
             _eventService = eventService;
             _roomService = roomService;
-            _userManager = userManager;
             _profileService = profileService;
         }
         
         [AllowAnonymous]
-        public ActionResult Index(string date)
+        public ActionResult Index(string eventId)
         {
-            return View();
+            var model = _eventService.GetEventsByDate(DateTime.Today)[0];
+            var rooms = _roomService.GetUnlockedRoomsByDate(model.StartTime);
+            var modelView = model.ToEventViewModel(rooms);
+            return View(modelView);
         }
 
         [HttpGet]
@@ -73,7 +74,7 @@ namespace SofthemeRoomBooking.Controllers
                 var rooms = _roomService.GetUnlockedRoomsByDate(model.StartTime);
                 var modelView = model.ToEventViewModel(rooms);
 
-                return PartialView("_EditEventPartial", modelView);
+                return PartialView("EditEvent", modelView);
             }
             ModelState.AddModelError("", "Что-то пошло не так");
 
@@ -83,6 +84,45 @@ namespace SofthemeRoomBooking.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult EditEvent(EventViewModel modelView)
+        {
+            var model = modelView.ToEventModel();
+
+            if (ModelState.IsValid)
+            {
+                if (_roomService.IsBusyRoom(model.IdRoom, model.StartTime, model.FinishTime))
+                {
+                    return Json(new { errorMessage = "Эта аудитория занята на выбраное время. Выберите, пожалуйста, другое." });
+                }
+
+                _eventService.UpdateEvent(model);
+
+                return Json(new { redirectTo = Url.Action("Index", "Home") });
+            }
+            ModelState.AddModelError("", "Что-то пошло не так");
+
+            return PartialView("_EditEventPartial", modelView);
+        }
+
+        [HttpGet]
+        public ActionResult EditEventPartial(int eventId)
+        {
+            var model = _eventService.GetEventById(eventId);
+
+            if (model != null)
+            {
+                var rooms = _roomService.GetUnlockedRoomsByDate(model.StartTime);
+                var modelView = model.ToEventViewModel(rooms);
+
+                return PartialView("_EditEventPartial", modelView);
+            }
+            ModelState.AddModelError("", "Что-то пошло не так");
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditEventPartial(EventViewModel modelView)
         {
             var model = modelView.ToEventModel();
 
@@ -130,7 +170,8 @@ namespace SofthemeRoomBooking.Controllers
         }
 
         [HttpPost]
-        public ActionResult CancelEvent(int id)
+        [ActionName("CancelEvent")]
+        public ActionResult CancelEventConfirm(int id)
         {
             _eventService.CancelEvent(id);
             return Content(@"Home/Index");
