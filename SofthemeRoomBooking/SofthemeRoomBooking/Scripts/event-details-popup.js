7a0cd5496893df8a1aa67949b1bdc2ba27c95080
Danil Shypik <Udavster@gmail.com>
2016-12-19 14:08:53 +0200

    Fix: scripts to bundle, styles for calendar (#74)

---
+++ SofthemeRoomBooking/SofthemeRoomBooking/Scripts/event-details-popup.js
@@ -0,0 +1,13 @@
+﻿$(document).on('click', '#cancelevent', function () {
+    var $this = $(this);
+    var id = $this.data('id');
+    $.ajax({
+        url: window.location.origin + "/Event/CancelEventView",
+        data: 'id=' + id,
+        type: 'GET',
+        success: function (res) {
+            $('#confirmation_popup').html(res)
+                                    .show();
+        }
+    });
+});
