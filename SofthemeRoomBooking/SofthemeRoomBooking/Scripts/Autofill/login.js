6b66e41b85ae203f2cc0e4c701db422466df3f78
Danil Shy <udavster@gmail.com>
2016-12-21 10:23:36 +0200

    Added autofill on register

---
+++ SofthemeRoomBooking/SofthemeRoomBooking/Scripts/Autofill/login.js
@@ -0,0 +1,12 @@
+﻿$(document)
+    .ready(function() {
+        $(".form__label")
+            .click(function(evt) {
+                console.log(evt);
+                if (evt.altKey) {
+                    console.log('Autofill');
+                    $('#Email').val('');
+                    $('#Password').val('');
+                }
+            });
+    });
